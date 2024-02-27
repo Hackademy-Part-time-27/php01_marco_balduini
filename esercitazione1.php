ed0a3bdfbbee2369aec5a889d8c979adadb2e307
Marco Balduini <marco.balduini93@gmail.com>
2024-02-27 21:21:30 +0100

    modifica traccia 3

--- esercitazione1.php
+++ esercitazione1.php
@@ -67,5 +67,5 @@ $words1 = [
     ]
     ];
 
-    $results = $words1[11] . " " . $words1[9]  . " " . $words1[20] . " " . $words1[10] . " " . $words1[20] . " " . $words2['elemento2'] . " " . $words1[2] . " " . $words1[4] . " " . $words1[17] . " " . $words1[18] . " " . $words1[0] . " " . $words1[12] . $words1[21] . " " . $words2['elemento3'][2] . " " . $words1[13] . " " . $words1[19] . " " . $words1[14] . " " . $words1[7] . " " . $words2['elemento3'][1] . $words2['elemento3'][3];    
+    $results = $words1[11] . " " . $words1[9]  . " " . $words1[20] . " " . $words1[10] . " " . $words1[20] . " " . $words2['elemento2'] . " " . $words1[2] . " " . $words1[4] . " " . $words1[17] . " " . $words1[18] . " " . $words1[0] . " " . $words1[12] . " " . $words1[6] . $words1[21] . " " . $words2['elemento3'][2] . " " . $words1[13] . " " . $words1[19] . " " . $words1[14] . " " . $words1[7] . " " . $words2['elemento3'][1] . $words2['elemento3'][3];    
     echo $results;
